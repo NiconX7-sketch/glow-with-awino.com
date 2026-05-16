22b7868cb1f2f2359780de6e56e0daa68d127326
NiconX7-sketch <nicodemuschool07@gmail.com>
2026-05-16 10:55:15 +0000

    Add files via upload

---
+++ js/API/test.js
@@ -0,0 +1,8 @@
+// api/test.js
+export default function handler(req, res) {
+    res.status(200).json({ 
+        status: 'ok', 
+        message: 'Vercel function is working!',
+        time: new Date().toISOString()
+    });
+}
